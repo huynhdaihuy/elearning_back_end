a0beba8ec0fa62eb13fec81d60a991ef07f9b48f
huynhdaihuy-friend <huynhdaihuy-friend@gmail.com>
2024-09-21 23:26:31 +0700

    refactor : re-structure app directory, adding integrating test

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from pymongo import MongoClient
+from app.app import create_app
+from app.config.config import TestConfig
+
+
+@pytest.fixture(scope='module')
+def client():
+    app = create_app(TestConfig)
+    with app.test_client() as client:
+        with app.app_context():
+            yield client
+
+
+@pytest.fixture(scope='module')
+def mongo():
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.test_elearning_db
+    yield db
+    client.drop_database('test_elearning_db')
